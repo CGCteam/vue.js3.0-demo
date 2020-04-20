cb0bc51bdf2ec9f4f8b8571d541af78cb64f957d
wangzhongjie <uvdream@163.com>
2020-04-20 14:59:02 +0800

    init🎉 初次提交

---
+++ src/components/input.vue
@@ -0,0 +1,29 @@
+<!--
+ * @Author: wangzhongjie
+ * @Date: 2020-04-20 09:34:43
+ * @LastEditors: wangzhongjie
+ * @LastEditTime: 2020-04-20 14:51:01
+ * @Description: 输入框
+ * @Email: UvDream@163.com
+ -->
+<template>
+  <div>
+    <input :type="type" v-model="value" @change="inputChange" />
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    type: String,
+    value: {}
+  },
+  setup(props, { emit }) {
+    const inputChange = e => {
+      console.log(e.target.value);
+      emit("input", e.target.value);
+    };
+    console.log(props);
+    return { props, inputChange };
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,50 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    {{ message }}
+    <div>
+      <p>基础</p>
+      <button @click="addFunc">+</button>
+      {{ count }}
+      <button @click="lessFunc">-</button>
+    </div>
+    <div>
+      组件
+      <JInput :type="data.type" :value="count" />
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import { ref, onMounted, onBeforeMount, onUpdated, reactive } from "vue";
+import JInput from "../components/input";
 export default {
   name: "Home",
-  components: {
-    HelloWorld
-  }
+
+  setup() {
+    const data = reactive({
+      type: "number"
+    });
+    // ref==useState
+    const count = ref(0);
+    // const val = ref("输入框初始化");
+    const addFunc = () => {
+      count.value++;
+    };
+    const lessFunc = () => {
+      count.value--;
+    };
+    onMounted(() => {
+      console.log("mounted -> onMounted");
+    });
+    onBeforeMount(() => {
+      console.log("beforeMount -> onBeforeMount");
+    });
+    onUpdated(() => {
+      console.log("updated -> onUpdated");
+      console.log(count.value);
+    });
+    return { message: "hello", addFunc, lessFunc, count, data };
+  },
+  components: { JInput }
 };
 </script>
